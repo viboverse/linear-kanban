75543b7aad456745acf6a7b5689a4aab8ff9090e
Vibo <vahab.afsharian94@gmail.com>
2025-12-15 20:52:07 +0200

    feat: We are properly connecte to NEON with the Prisma 7 config, finally!!

---
+++ prisma/migrations/20251215184535_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('TODO', 'IN_PROGRESS', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'TODO',
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "priority" "Priority" NOT NULL DEFAULT 'LOW',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "dueDate" TIMESTAMP(3),
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
