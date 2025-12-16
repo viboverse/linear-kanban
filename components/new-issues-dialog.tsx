056906551614911b2d8b9f8046487e11e0fa7188
Vibo <vahab.afsharian94@gmail.com>
2025-12-16 12:53:52 +0200

    fix: Remove unused import from NewIssueDialog component

--- components/new-issues-dialog.tsx
+++ components/new-issues-dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CirclePlus, Divide } from "lucide-react";
+import { CirclePlus } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
