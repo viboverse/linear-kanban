a9ba086d173e1368237752eb3b675b3f545cc4e8
Vibo <vahab.afsharian94@gmail.com>
2025-12-16 19:37:47 +0200

    fix: correct task filtering logic in Board component

--- components/board.tsx
+++ components/board.tsx
@@ -4,13 +4,13 @@ import { Task } from "@prisma/client";
 export function Board({ tasks }: { tasks: Task[] }) {
   const todoTasks = tasks.filter((task) => task.status === "TODO");
   const inProgressTasks = tasks.filter((task) => task.status === "IN_PROGRESS");
-  const doneTasks = tasks.filter((task) => task.status === "TODO");
+  const doneTasks = tasks.filter((task) => task.status === "DONE");
 
   return (
     <div className="grid h-screen w-full grid-cols-3 gap-2 bg-blue-600 px-1">
-      <Column title="Todo" tasks={doneTasks} />
+      <Column title="Todo" tasks={todoTasks} />
       <Column title="In Progress" tasks={inProgressTasks} />
-      <Column title="Done" tasks={todoTasks} />
+      <Column title="Done" tasks={doneTasks} />
     </div>
   );
 }
