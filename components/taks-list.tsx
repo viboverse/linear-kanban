c6cf688d62507454556d3da66a91f8cfc7ef4849
Vibo <vahab.afsharian94@gmail.com>
2025-12-16 19:36:47 +0200

    refactor: extract TaskListItem component and improve task list renderingrefactor:

--- components/taks-list.tsx
+++ components/taks-list.tsx
@@ -1,7 +1,7 @@
 import { Task } from "@prisma/client";
 import TaskListItem from "./task-list-item";
 
-export default function TaskList({ tasks }: { tasks: Task[] }) {
+export default async function TaskList({ tasks }: { tasks: Task[] }) {
   if (tasks.length === 0) {
     return <p className="text-center font-bold">No Tasks yet</p>;
   }
@@ -9,13 +9,7 @@ export default function TaskList({ tasks }: { tasks: Task[] }) {
   return (
     <ul className="flex flex-col gap-2">
       {tasks.map((task) => (
-        <TaskListItem
-          key={task.id}
-          title={task.title}
-          dueDate={task.dueDate}
-          priority={task.priority}
-          status={task.status}
-        />
+        <TaskListItem key={task.id} task={task} />
       ))}
     </ul>
   );
